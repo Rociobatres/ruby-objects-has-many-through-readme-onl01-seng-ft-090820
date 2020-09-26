f5af41290fe8f5d85a797e2ace90d9d9b1c7d04d
Rocio Batres <jonasgirl21@gmail.com>
2020-09-26 15:59:20 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,9 +2,16 @@ class Waiter
   
   attr_accessor :name, :years_worked
   
+  @@all = [] 
+  
   def initialize(name, years_worked)
     @name = name 
     @years_worked = years_worked 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
 
 end
