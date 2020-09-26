7f8ca8971889f51e057cc1067c8f461fe9d15d57
Rocio Batres <jonasgirl21@gmail.com>
2020-09-26 16:01:23 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  @@all = []
+  
+  def initialize(waiter,cutomer,total,tip)
+    @waiter = waiter
+    @cutomer = cutomer
+    @total = total 
+    @tip = tip 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 
 end
