2f736663bc9272027da608f033573a1193968bb6
Rocio Batres <jonasgirl21@gmail.com>
2020-09-26 15:50:02 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,6 +2,15 @@ class Customer
   
 attr_accessor :name :age 
 
-def intialize ()
+def intialize (name, age)
+  @name = name 
+  @age  = age 
+end 
+
+def self.all 
+  @@all = []
+  @@all << self.new 
+  @all 
+end 
   
 end
